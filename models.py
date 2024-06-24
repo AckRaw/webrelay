1b6863885fd12ee384c7129fd506afcdf9d41dcf
AckRaw <129283456+AckRaw@users.noreply.github.com>
2024-06-24 23:58:41 +0200

    Update models.py

--- models.py
+++ models.py
@@ -6,8 +6,3 @@ class User(UserMixin, db.Model):
     login = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-'''
-class pins(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True) # primary keys are required by SQLAlchemy
-    name = db.Column(db.String(1000))
-'''
