18666557707b5099608e4e72fe7d5967e6cf37c2
AckRaw <129283456+AckRaw@users.noreply.github.com>
2024-06-24 23:57:10 +0200

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,55 @@
+#
+#export FLASK_APP=webrelay
+#export FLASK_DEBUG=1
+#
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from werkzeug.security import generate_password_hash
+
+# init SQLAlchemy so we can use it later in our models
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    with app.app_context():
+        db.create_all()    
+
+    with app.app_context():
+        user = User.query.filter_by(login='admin').first()
+
+        # if admin user don't exist, create it
+        if not user:
+            new_user = User(login="admin", name="Admin", password=generate_password_hash("first", method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+
+    from . import models
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
